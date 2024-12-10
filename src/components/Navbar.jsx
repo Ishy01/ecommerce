3beaf0d1671680957f601d863d778ca912303f4c
Ishmael Robertson Tettedzie <irtettedzie554@gmail.com>
2024-12-10 10:41:19 +0000

    nav links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "../../src/assets/logo.png";
 import { IoMdSearch } from "react-icons/io";
-import { FaCartShopping } from "react-icons/fa6";
+import { FaCaretDown, FaCartShopping } from "react-icons/fa6";
 import DarkMode from "./DarkMode";
 
 const Menu = [
@@ -32,6 +32,24 @@ const Menu = [
   },
 ]
 
+const DropdownLinks = [
+  {
+    id: 1,
+    name: "Trending Products",
+    link: "#"
+  },
+  {
+    id: 2,
+    name: "Best Selling",
+    link: "#"
+  },
+  {
+    id: 1,
+    name: "Top Rated",
+    link: "#"
+  },
+]
+
 const Navbar = () => {
   return (
     <div className="shadow-md bg-white dark:border-gray-900 dark:text-white duration-200 relative z-40 ">
@@ -48,9 +66,9 @@ const Navbar = () => {
               <input
                 type="text"
                 placeholder="search"
-                className="w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none focus:border-1 focus:border-primary"
+                className="w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none focus:border-1 focus:border-primary dark:border-gray-500 dark:bg-gray-500"
               />
-              <IoMdSearch className="text-gray-500 group-hover:text-primary absolute top-1/2 -translate-y-1/2 right-3" />
+              <IoMdSearch className="text-gray-500 group-hover:text-primary absolute top-1/2 -translate-y-1/2 right-3 dark:text-primary" />
             </div>
             <button
               onClick={() => alert("Ordering not available yet")}
@@ -68,15 +86,38 @@ const Navbar = () => {
         </div>
       </div>
       {/* lower Navbar */}
-      <div>
-        <ul className="">
+      <div className="flex justify-center bg-white dark:bg-gray-900 duration-200">
+        <ul className="sm:flex hidden items-center gap-4">
           {
             Menu.map((data) => (
               <li key={data.id}>
-                <a href={data.link}>{data.name}</a>
+                <a href={data.link} className="inline-block px-4 hover:text-primary duration-200">{data.name}</a>
               </li>
             ))
           }
+          <li className="group relative cursor-pointer">
+            <a href="#" className="flex items-center gap-[2px] py-2">
+              Trending Products
+              <span>
+                <FaCaretDown
+                className="transition-all duration-200 group-hover:rotate-100"
+                />
+              </span>
+            </a>
+            <div className="absolute z-[9999] hidden group-hover:block w-{150px] rounded-md bg-white dark:bg-gray-900 p-2 text-black dark:text-white shadow-md">
+              <ul>
+                {
+                  DropdownLinks.map((data) => (
+                    <li key={data.id}>
+                      <a href={data.link} className="inline-block w-full rounded-md p-2 hover:bg-primary/20">
+                        {data.name}
+                      </a>
+                    </li>
+                  ))
+                }
+              </ul>
+            </div>
+          </li>
         </ul>
       </div>
     </div>
